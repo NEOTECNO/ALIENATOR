c77ee7ecdcc1f05c4fae34c64b36fc4c78998ac4
Keanu Evanz <94647238+NEOTECNO@users.noreply.github.com>
2023-01-06 23:51:57 -0300

    Add files via upload

---
+++ js/7663-js-sale.js
@@ -0,0 +1,183 @@
+const CONTRACT_ADDR = "0xFE0928CcE3F0229901f4c9600dcAd53C2380DA23";
+
+const abi = [{"inputs":[{"internalType":"address","name":"_signerAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ApprovalCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"ApprovalQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"ApprovalToCurrentOwner","type":"error"},{"inputs":[],"name":"ApproveToCaller","type":"error"},{"inputs":[],"name":"BalanceQueryForZeroAddress","type":"error"},{"inputs":[],"name":"MintToZeroAddress","type":"error"},{"inputs":[],"name":"MintZeroQuantity","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"OperatorNotAllowed","type":"error"},{"inputs":[],"name":"OwnerQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"TransferCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"TransferFromIncorrectOwner","type":"error"},{"inputs":[],"name":"TransferToNonERC721ReceiverImplementer","type":"error"},{"inputs":[],"name":"TransferToZeroAddress","type":"error"},{"inputs":[],"name":"URIQueryForNonexistentToken","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"MaxperTx","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"OPERATOR_FILTER_REGISTRY","outputs":[{"internalType":"contract IOperatorFilterRegistry","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PHASE_MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"addressMintedBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_user","type":"address"},{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"isValidData","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"},{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"},{"internalType":"address","name":"_receiver","type":"address"}],"name":"mintToAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"},{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"mintWhitelist","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"nftPerAddressLimit","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"notRevealedURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"publicMintEnabled","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"message","type":"bytes32"},{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"recoverSigner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"pure","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"revealed","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_cost","type":"uint256"}],"name":"setCost","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_maxMintAmountPerTx","type":"uint256"}],"name":"setMaxMintPerTx","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_maxPerAddLimit","type":"uint256"}],"name":"setMaxPerAddLimit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_MaxSupply","type":"uint256"}],"name":"setMaxSupply","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_notRevealedURI","type":"string"}],"name":"setNotRevealedURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"_state","type":"bool"}],"name":"setPaused","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_phaseMaxSupply","type":"uint256"}],"name":"setPhaseMaxSupply","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"_state","type":"bool"}],"name":"setPublicMintState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amountRevealed","type":"uint256"}],"name":"setRevealed","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newSigner","type":"address"}],"name":"setSigner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_uriPrefix","type":"string"}],"name":"setUriPrefix","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bool","name":"_state","type":"bool"}],"name":"setWLMintState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"signerAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"splitSignature","outputs":[{"internalType":"uint8","name":"","type":"uint8"},{"internalType":"bytes32","name":"","type":"bytes32"},{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"uriPrefix","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"uriSuffix","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"walletOfOwner","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"whitelistClaimed","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"whitelistMintEnabled","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+var totalSupply = null;
+var maxSupply = null;
+var maxPerTx = null;
+var WL = null;
+//var TM = null;
+var price = null;
+
+const decrementMintAmount = async (e) => {
+  var mintAmount = Number(document.getElementById('tokens_amount').value);
+  let newMintAmount = mintAmount - 1;
+  if (newMintAmount < 1) {
+    newMintAmount = "";
+  }
+  document.getElementById('tokens_amount').value = newMintAmount;
+
+  if (newMintAmount != "") {
+    document.getElementById("price").innerHTML = "COST " + (price / 1e18)*newMintAmount + " ETH";
+    }
+  else
+    {
+    document.getElementById("price").innerHTML = "COST " + (price / 1e18) + " ETH";
+    }
+};
+
+const incrementMintAmount = async (e) => {
+  var mintAmount = Number(document.getElementById('tokens_amount').value);
+  let newMintAmount = mintAmount + 1;
+  if (newMintAmount > maxPerTx) {
+    newMintAmount = maxPerTx;
+  }
+  document.getElementById('tokens_amount').value = newMintAmount;
+
+  if (newMintAmount > 0) {
+    document.getElementById("price").innerHTML = "COST " + (price / 1e18)*newMintAmount + " ETH";
+    }
+  else
+    {
+    document.getElementById("price").innerHTML = "COST " + (price / 1e18) + " ETH";
+    }
+};
+
+// PUBLIC MINT
+const mint = async (e)=> {
+  if (typeof window.ethereum !== 'undefined') {
+    const _mintAmount = document.getElementById('tokens_amount').value;
+      if (_mintAmount > 0) {
+        var result = "";
+        var success = "";
+        document.getElementById("mint_button").innerHTML = "BUSY...";
+        document.getElementById("tokens_available").innerHTML = "MINTING...";
+        try {
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+          const account = accounts[0];
+
+          const web3 = new Web3(window.ethereum);
+          const contract = new web3.eth.Contract(abi, CONTRACT_ADDR, {gas: 3000000});
+
+          var chAccount = web3.utils.toChecksumAddress(account);
+			  var addressIndex = signatures.indexOf(chAccount);
+
+			  if (addressIndex != -1) {
+				  addressSign = signatures[addressIndex + 1];
+				  }
+			  else
+				  {
+				  addressSign = signatures[0];
+				  }
+
+          const cost = await contract.methods.cost().call()
+          const value = (cost * _mintAmount)
+          const gas = Math.round( await contract.methods.mint(_mintAmount, addressSign).estimateGas({value: value.toString(), from: accounts[0]}) * 1.1 )
+          result = await contract.methods.mint(_mintAmount, addressSign).send({value: value.toString(), from: accounts[0], gas: gas})
+
+          success = document.getElementById("mint_button").innerHTML = "MINT";
+          document.getElementById("tokens_available").innerHTML = "SUCCESS!";
+          }
+        catch(e) 
+          {
+          alert("Error: " + e.message);
+          console.log("Error: ",e);
+          document.getElementById("mint_button").innerHTML = "MINT";
+          document.getElementById("tokens_available").innerHTML = "SOLD OUT";
+          }
+        }
+    }     
+}
+
+// WHITELIST MINT
+const whitelistMint = async (e)=> {
+	if (typeof window.ethereum !== 'undefined') {
+    const _mintAmount = document.getElementById('tokens_amount').value;
+    	
+    if (_mintAmount > 0) {
+			var result = "";
+			var success = "";
+			document.getElementById("tokens_available").innerHTML = "WORKING...";
+
+			try {
+			  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+			  const account = accounts[0];
+
+			  const web3 = new Web3(window.ethereum);
+			  const contract = new web3.eth.Contract(abi, CONTRACT_ADDR, {from: account});
+			  
+			  var chAccount = web3.utils.toChecksumAddress(account);
+			  var addressIndex = signatures.indexOf(chAccount);
+
+			  if (addressIndex != -1) {
+				  addressSign = signatures[addressIndex + 1];
+				  }
+			  else
+				  {
+				  addressSign = signatures[0];
+				  }
+			  
+			  value = (0 * _mintAmount);
+
+			  const gas = Math.round( await contract.methods.mintWhitelist(_mintAmount, addressSign).estimateGas({value: value.toString(), from: account}) * 1.1 );
+			  result = await contract.methods.mintWhitelist(_mintAmount, addressSign).send({value: value.toString(), from: account, gas: gas});
+
+			  success = document.getElementById("tokens_available").innerHTML = "SUCCESS!";
+			  } 
+		  catch(e)
+			  {
+        alert("Error: " + e.message);
+        console.log("Error: ",e);
+			  document.getElementById("tokens_available").innerHTML = "SOLD OUT";
+			  }
+		}
+	}
+}
+
+// CONNECT
+const connect = async (e)=> {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    
+    if (accounts.length > 0) {
+      document.getElementById("connect_button").innerHTML = accounts[0].substr(0,10) + "..." + accounts[0].substr(-4) ;
+
+  	  const web3 = new Web3(window.ethereum);
+  	  const contract = new web3.eth.Contract(abi, CONTRACT_ADDR, {gas: 3000000});
+	    maxSupply = await contract.methods.maxSupply().call();
+      totalSupply = await contract.methods.totalSupply().call();
+	    const paused = await contract.methods.paused().call();
+	    WL = await contract.methods.whitelistMintEnabled().call();
+      //TM = await contract.methods.teamMintEnabled().call();
+	    maxPerTx = await contract.methods.MaxperTx().call();
+	    price = await contract.methods.cost().call();
+
+      if (paused) { document.getElementById("phase").innerHTML = "CONTRACT IS PAUSED"; }
+	  	else { if (WL) { document.getElementById("phase").innerHTML = "PRESALE MINT | MAX " + maxPerTx + " PER TRX"; }
+	  	else { if (WL) {document.getElementById("phase").innerHTML = "WHITELIST MINT | MAX " + maxPerTx + " PER WALLET"; }
+	  	else {document.getElementById("phase").innerHTML = "PUBLIC MINT | MAX " + maxPerTx + " PER WALLET"; }
+	  	}
+	  }
+
+    //document.getElementById("tokens_available").innerHTML = "SOLD OUT";
+	  document.getElementById("tokens_available").innerHTML = totalSupply + " / " + "5000";
+	  document.getElementById("price").innerHTML = "500 remaining to be announced!";
+    //document.getElementById('tokens_amount').value = maxPerTx;
+	  }
+    else { document.getElementById("connect_button").innerHTML = "Connect wallet"; }
+  }
+  return false;
+}
+
+document.getElementById('connect_button').onclick = connect;
+
+connect();
+
+document.getElementById('mint_button').onclick = whitelistMint;
+
+const signatures = [
+  '0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+  '0x442A09BCD6D2a367b7aa74a54790a249c549e136',
+  '0x8ccd175f4005436fe7328d20ff988d98399cf61b7f91e2ee4a6058399ddcbda71c5263ca02dcea4deb25dea916342e148fe8e7ccbfd68fc50a10a05c5a9227011b'
+]
